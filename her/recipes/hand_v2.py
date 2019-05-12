cb775a6e7fe40b35b93543d2238ce559f22ecdc8
Özsel Kılınç <ozsel@ozsel-mac.wifi-campus.warwick.ac.uk>
2019-05-12 05:05:36 +0100

    Qintr*2 for rotations

--- her/recipes/hand_v2.py
+++ her/recipes/hand_v2.py
@@ -153,7 +153,7 @@ for env_name in env_name_list:
                 rob_name = rob_name + '_DDPG_'
 
 
-        path = './models_paper/batch/' + rob_name + '_' + str(i_exp)
+        path = './models_paper/batch/' + rob_name + str(i_exp)
         try:  
             os.makedirs(path)
         except OSError:  
@@ -171,6 +171,6 @@ for env_name in env_name_list:
         with open(path + '/normalizer.pkl', 'wb') as file:
             pickle.dump(normalizer2, file)
 
-        path = './monitors_paper/batch/monitor_' + rob_name  + '_' + str(i_exp) + '.npy'
+        path = './monitors_paper/batch/monitor_' + rob_name + str(i_exp) + '.npy'
         np.save(path, monitor2)
 
