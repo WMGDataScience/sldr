b03e0d5e21e2676d079aad6eb8a07f985e4c5ee0
Özsel Kılınç <ozsel@ozsel-mac.local>
2019-10-01 20:58:18 +0100

    adding render 1 and cosmetical changes

--- her/main_q.py
+++ her/main_q.py
@@ -63,7 +63,7 @@ def init(config, agent='robot', her=False, object_Qfunc=None,
                                     observe_obj_grp=config['observe_obj_grp'],
                                     obj_range=config['obj_range'])
         envs = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(N_ENVS)])
-        envs_test = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(1)])
+        envs_render = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(1)])
         n_rob_actions = 4
         n_actions = config['max_nb_objects'] * len(config['obj_action_type']) + n_rob_actions
     elif 'Fetch' in ENV_NAME and 'Multi' in ENV_NAME and 'Flex' in ENV_NAME:
@@ -72,19 +72,19 @@ def init(config, agent='robot', her=False, object_Qfunc=None,
                                     observe_obj_grp=config['observe_obj_grp'],
                                     obj_range=config['obj_range'])
         envs = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(N_ENVS)])
-        envs_test = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(1)])
+        envs_render = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Fetch') for i_env in range(1)])
         n_rob_actions = 4
         n_actions = 2 * len(config['obj_action_type']) + n_rob_actions
     elif 'HandManipulate' in ENV_NAME and 'Multi' in ENV_NAME:
         dummy_env = gym.make(ENV_NAME, obj_action_type=config['obj_action_type'])
         envs = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Hand') for i_env in range(N_ENVS)])
-        envs_test = None
+        envs_render = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Hand') for i_env in range(1)])
         n_rob_actions = 20
         n_actions = 1 * len(config['obj_action_type']) + n_rob_actions
     else:
         dummy_env = gym.make(ENV_NAME)
         envs = SubprocVecEnv([make_env(ENV_NAME, i_env, 'Others') for i_env in range(N_ENVS)])
-        envs_test = None
+        envs_render = None
 
     def her_reward_fun(ag_2, g, info):  # vectorized
         return dummy_env.compute_reward(achieved_goal=ag_2, desired_goal=g, info=info)
@@ -131,7 +131,8 @@ def init(config, agent='robot', her=False, object_Qfunc=None,
                   Actor, Critic, loss_func, GAMMA, TAU, out_func=OUT_FUNC, discrete=False, 
                   regularization=REGULARIZATION, normalized_rewards=NORMALIZED_REWARDS,
                   agent_id=agent_id, object_Qfunc=object_Qfunc, backward_dyn=backward_dyn, 
-                  object_policy=object_policy, reward_fun=reward_fun, clip_Q_neg=clip_Q_neg)
+                  object_policy=object_policy, reward_fun=reward_fun, clip_Q_neg=clip_Q_neg,
+                  )
     normalizer = [Normalizer(), Normalizer()]
 
     for _ in range(1):
@@ -173,7 +174,7 @@ def init(config, agent='robot', her=False, object_Qfunc=None,
         }
     memory = ReplayBuffer(buffer_shapes, MEM_SIZE, config['episode_length'], sample_her_transitions)
 
-    experiment_args = ((envs,envs_test), memory, noise, config, normalizer, None)
+    experiment_args = ((envs, envs_render), memory, noise, config, normalizer, None)
 
     print('no normaliser update') 
 
@@ -192,7 +193,7 @@ def back_to_dict(state, config):
 
     return state_dict
 
-def rollout(env, model, noise, config, normalizer=None, render=False, running_rintr_mean=None,):
+def rollout(env, model, noise, config, normalizer=None, render=False):
     trajectories = []
     for i_agent in range(2):
         trajectories.append([])
@@ -299,7 +300,7 @@ def run(model, experiment_args, train=True):
     total_time_start =  time.time()
 
     envs, memory, noise, config, normalizer, _ = experiment_args
-    env, env_test = envs
+    envs_train, envs_render = envs
     
     N_EPISODES = config['n_episodes'] if train else config['n_episodes_test']
     N_CYCLES = config['n_cycles']
@@ -322,8 +323,7 @@ def run(model, experiment_args, train=True):
         if train:
             for i_cycle in range(N_CYCLES):
                 
-                #i_env = i_rollout % config['n_envs']
-                trajectories, _, _, _ = rollout(env, model, noise, config, normalizer, render=False)
+                trajectories, _, _, _ = rollout(envs_train, model, noise, config, normalizer, render=False)
                 memory.store_episode(trajectories.copy())   
               
                 for i_batch in range(N_BATCHES):  
@@ -345,15 +345,15 @@ def run(model, experiment_args, train=True):
         rollout_per_env = N_TEST_ROLLOUTS // config['n_envs']
         for i_rollout in range(rollout_per_env):
             render = config['render'] == 2 and i_episode % config['render'] == 0
-            _, episode_reward, success, _ = rollout(env, model, False, config, normalizer=normalizer, render=render)
+            _, episode_reward, success, _ = rollout(envs_train, model, False, config, normalizer=normalizer, render=render)
                 
             episode_reward_cycle.extend(episode_reward)
             episode_succeess_cycle.extend(success)
 
-        render = (config['render'] == 1) and (i_episode % config['render'] == 0) and (env_test is not None)
+        render = (config['render'] == 1) and (i_episode % config['render'] == 0) and (envs_render is not None)
         if render:
             for i_rollout in range(10):
-                _, episode_reward, success, _ = rollout(env_test, model, False, config, normalizer=normalizer, render=render)
+                _, episode_reward, success, _ = rollout(envs_render, model, False, config, normalizer=normalizer, render=render)
         # <-- end loop: i_rollout 
             
         ### MONITORIRNG ###
